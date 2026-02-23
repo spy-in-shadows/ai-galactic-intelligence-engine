dac5a6b0dc3be34965b360dc47842893b24b527e
Krishna Verma <krishnaias1289@gmail.com>
2026-02-24 01:30:07 +0530

    Backend now serves cached local SWAPI data

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+def load_data(file):
+    with open(f"../data/{file}.json") as f:
+        return json.load(f)
 
 @app.get("/")
 def root():
     return {"status": "Backend running"}
+
+@app.get("/planets")
+def get_planets():
+    return load_data("planets")
+
+@app.get("/characters")
+def get_characters():
+    return load_data("characters")
+
+@app.get("/films")
+def get_films():
+    return load_data("films")
